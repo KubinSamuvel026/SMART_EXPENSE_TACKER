8f68943c984928325dd1527c1e325cb58c37f08a
Kubin samuvel <kubinsamuvel@gmail.com>
2026-02-01 21:34:20 +0530

    Fix import statement for health view

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import RegisterView,CustomTokenObtainPairView,health
-from .views import RegisterView.health
+from .views import health
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
