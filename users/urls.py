ff21b7f87a4621f1f833482e8e9d28ed3f9a1a81
Kubin samuvel <kubinsamuvel@gmail.com>
2026-02-01 21:20:21 +0530

    Add health check endpoint to URLs

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RegisterView,CustomTokenObtainPairView
+from .views import RegisterView,CustomTokenObtainPairView,health
 
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('login/', CustomTokenObtainPairView.as_view()),
+     path("health/", health),
 ]
